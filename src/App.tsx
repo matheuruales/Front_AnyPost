235aa7520c1eaa6417f2373c2d2e521a86dda765
Matheu2b2t <johnatan.ruales@campusucc.edu.co>
2025-11-17 04:13:22 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ const App: React.FC = () => {
             path="/dashboard"
             element={
               <ProtectedRoute>
-                <Historial />
+                <Dashboard />
               </ProtectedRoute>
             }
           />
